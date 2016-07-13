9e5e8fbca6fe678dc7beb07ad32d7dedd25b9307
JuliusPH <juliuscessarlapugot@gmail.com>
2016-07-13 10:35:15 +0800

    Add application context config

---
+++ spring/web/src/main/java/com/exist/config/ApplicationContextConfig.java
@@ -0,0 +1,53 @@
+package com.exist.config;
+
+import com.exist.model.Address;
+import com.exist.model.Contact;
+import com.exist.model.Person;
+import com.exist.model.Role;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAspectJAutoProxy
+@EnableTransactionManagement
+@ComponentScan({"com.exist.dao", "com.exist.service"})
+public class ApplicationContextConfig{
+    @Bean(name = "dataSource")
+    public BasicDataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost/persondb");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("121212");
+        return dataSource;
+    }
+
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(BasicDataSource dataSource) {
+        LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+        sessionBuilder.addAnnotatedClasses(Person.class, Address.class, Contact.class, Role.class);
+        sessionBuilder.setProperty("hibernate.show_sql", "true");
+        sessionBuilder.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        sessionBuilder.setProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory");
+        sessionBuilder.setProperty("hibernate.cache.provider_class", "net.sf.ehcache.hibernate.EhCacheProvider");
+        sessionBuilder.setProperty("hibernate.cache.use_second_level_cache", "true");
+        sessionBuilder.setProperty("hibernate.cache.use_query_cache", "true");
+        return sessionBuilder.buildSessionFactory();
+    }
+
+    @Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+        return transactionManager;
+    }
+}
